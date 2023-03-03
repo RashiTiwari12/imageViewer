58cf953ed47e2abc560560960b51230bd8033ccc
Rashi Tiwari <117359006+RashiTiwari12@users.noreply.github.com>
2023-03-03 21:38:41 +0530

    changes done

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+
+ let data = document.getElementById("i-box");
 let img=document.getElementById("nature-image")
-function ok() {
-    let valo = document.getElementById("i-box").value;
-   img.src = `https://source.unsplash.com/900x900?${valo}`;
+let submit = document.getElementById("submit");
+function handleSubmit() {
+   img.src = `https://source.unsplash.com/900x900?${data.value}`;
+   submit.style.visibility ="hidden"
 }
 
-let submit = document.getElementsByClassName("submit");
-submit.addEventListener('click', ok);
+function handleRefresh(){
+    localStorage.setItem("data",data.value)
+    localStorage.setItem("isRefresh","true")
+    window.location.reload();
+   
+}
+
+
+if(localStorage.getItem("isRefresh") === "true" &&  localStorage.getItem("data").length >0) {
+    img.src = `https://source.unsplash.com/900x900?${localStorage.getItem("data")}`;
+    data.value =  localStorage.getItem("data")
+    localStorage.setItem("isRefresh","false")
+    submit.style.visibility ="hidden"
+}
